05ed80008d471bbebb545c41dc4a9ba985bf3dc2
Monika <mm24phc1r33@student.nitw.ac.in>
2026-02-08 15:05:45 +0530

    Update gpio.h

--- Task2/submission/gpio.h
+++ Task2/submission/gpio.h
@@ -1,1 +1,16 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#include "ch32v00x.h"
+
+/* CHANGE THIS AS PER DATASHEET */
+#define LED_GPIO_PORT GPIOD
+#define LED_GPIO_PIN  GPIO_Pin_6
+
+void gpio_init(void);
+void gpio_set(void);
+void gpio_clear(void);
+void gpio_toggle(void);
+
+#endif
 
