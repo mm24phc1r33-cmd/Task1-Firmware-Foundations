5d2b5b70942934f3496dfcb459e0461ea69b47ae
Monika <mm24phc1r33@student.nitw.ac.in>
2026-02-08 15:05:09 +0530

    Update main.c

--- Task2/submission/main.c
+++ Task2/submission/main.c
@@ -1,1 +1,47 @@
+#include "gpio.h"
+#include "uart.h"
+#include <stdio.h>
+#include "ch32v00x.h"
+
+// Function declaration
+void Delay_ms(uint32_t ms);
+
+int main(void)
+{
+    uint32_t counter = 0;
+    char buffer[64];
+
+    uart_init();
+    gpio_init();
+
+    uart_print("\r\n=====================\r\n");
+    uart_print("Counter 0 to 10 Started\r\n");
+    uart_print("=====================\r\n");
+
+    while (1)
+    {
+        // Check karein ki counter 10 tak pahuncha hai ya nahi
+        if (counter <= 10)
+        {
+            gpio_toggle(); // LED blink hogi
+
+            sprintf(buffer, "Counter = %lu\r\n", counter++);
+            uart_print(buffer);
+
+            Delay_ms(500); // 0.5 second ka delay
+        }
+        else
+        {
+            // 10 ke baad print ruk jayega, LED toggle band ho jayegi
+            // Agar aap chahte hain ki ginti phir se shuru ho, toh yahan counter = 0; likh sakte hain
+        }
+    }
+}
+
+// Software Delay Function
+void Delay_ms(uint32_t ms) {
+    for (uint32_t i = 0; i < ms * 2000; i++) {
+        __asm__("nop");
+    }
+}
 
