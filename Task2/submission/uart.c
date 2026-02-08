36e54181d68fb3cf5e52bb67195e00072c2dc659
Monika <mm24phc1r33@student.nitw.ac.in>
2026-02-08 15:04:25 +0530

    Update uart.c

--- Task2/submission/uart.c
+++ Task2/submission/uart.c
@@ -1,1 +1,38 @@
+#include "uart.h"
+#include "ch32v00x.h"
+
+void uart_init(void)
+{
+    GPIO_InitTypeDef gpio;
+    USART_InitTypeDef uart;
+
+    // PD5 aur USART1 ki clock enable karein
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_USART1, ENABLE);
+
+    // TX Pin setup: VSD Squadron Mini ke liye PD5 use hota hai
+    gpio.GPIO_Pin = GPIO_Pin_5;
+    gpio.GPIO_Mode = GPIO_Mode_AF_PP;
+    gpio.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOD, &gpio);
+
+    // UART Configuration
+    uart.USART_BaudRate = 115200;
+    uart.USART_WordLength = USART_WordLength_8b;
+    uart.USART_StopBits = USART_StopBits_1;
+    uart.USART_Parity = USART_Parity_No;
+    uart.USART_Mode = USART_Mode_Tx;
+    uart.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+
+    USART_Init(USART1, &uart);
+    USART_Cmd(USART1, ENABLE);
+}
+
+void uart_print(const char *str)
+{
+    while (*str)
+    {
+        while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+        USART_SendData(USART1, *str++);
+    }
+}
 
