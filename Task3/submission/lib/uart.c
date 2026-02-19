0edef80bcc4b288b57e9d715651bca3b43d15d7f
Monika <mm24phc1r33@student.nitw.ac.in>
2026-02-20 00:59:55 +0530

    Update uart.c

--- Task3/submission/lib/uart.c
+++ Task3/submission/lib/uart.c
@@ -1,1 +1,47 @@
+#include "uart.h"
+#include "ch32v00x.h"
+
+void uart_init(uint32_t baud)
+{
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_USART1, ENABLE);
+
+    GPIO_InitTypeDef gpio;
+
+    // TX PD5
+    gpio.GPIO_Pin = GPIO_Pin_5;
+    gpio.GPIO_Mode = GPIO_Mode_AF_PP;
+    gpio.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOD, &gpio);
+
+    // RX PD6
+    gpio.GPIO_Pin = GPIO_Pin_6;
+    gpio.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOD, &gpio);
+
+    USART_InitTypeDef uart;
+    uart.USART_BaudRate = baud;
+    uart.USART_WordLength = USART_WordLength_8b;
+    uart.USART_StopBits = USART_StopBits_1;
+    uart.USART_Parity = USART_Parity_No;
+    uart.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+    uart.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+
+    USART_Init(USART1, &uart);
+    USART_Cmd(USART1, ENABLE);
+}
+
+char uart_read_char(void)
+{
+    while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET);
+    return USART_ReceiveData(USART1);
+}
+
+void uart_print(const char *str)
+{
+    while(*str)
+    {
+        while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+        USART_SendData(USART1, *str++);
+    }
+}
 
