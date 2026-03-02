410e531e10fc84e6bb4257d76c4f6e238502b67d
Monika <mm24phc1r33@student.nitw.ac.in>
2026-03-02 11:08:23 +0530

    Update uart.h

--- Task4/submission/lib/uart.h
+++ Task4/submission/lib/uart.h
@@ -1,1 +1,12 @@
+#ifndef UART_H
+#define UART_H
+
+#include <stdint.h>
+
+void uart_init(uint32_t baudrate);
+void uart_write_char(char c);
+void uart_print(char *str);
+char uart_read_char(void);
+
+#endif
 
